eaacb733b95c7f27172641ce99fe34dce840e62d
Alex Banul <albanul92@gmail.com>
2015-04-08 02:48:01 +0300

    minor bug fixes

--- source/main.cpp
+++ source/main.cpp
@@ -253,11 +253,11 @@ void InitializeData() {
     for (int i = 1; i < n1; ++i) {
         for (int j = 1; j < n2; ++j) {
             for (int k = 1; k < n3; ++k) {
-                u1nCon->elem(i, j, k) = u10;
-                u2nCon->elem(i, j, k) = u20;
-                u3nCon->elem(i, j, k) = u30;
-                ronCon->elem(i, j, k) = ro0_g;
-                tnCon->elem(i, j, k) = t0;
+                u1Con->elem(i, j, k) = u1nCon->elem(i, j, k) = u10;
+                u2Con->elem(i, j, k) = u2nCon->elem(i, j, k) = u20;
+                u3Con->elem(i, j, k) = u3nCon->elem(i, j, k) = u30;
+                roCon->elem(i, j, k) = ronCon->elem(i, j, k) = ro0_g;
+                tCon->elem(i, j, k) = tnCon->elem(i, j, k) = t0;
             }
         }
     }
@@ -274,9 +274,9 @@ void InitializeData() {
         }
     }
 
-    for (int i = 1; i < n1; ++i) {
-        for (int j = 1; j < n2; ++j) {
-            for (int k = 1; k < n3; ++k) {
+    for (int i = 0; i <= n1; ++i) {
+        for (int j = 0; j <= n2; ++j) {
+            for (int k = 0; k <= n3; ++k) {
                 condition->elem(i, j, k) = 0;
             }
         }
